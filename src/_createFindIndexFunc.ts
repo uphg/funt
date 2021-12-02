1c6cbbe51753a128c09df813a0dc39852e521e5a
uphg <2633565885@qq.com>
2021-12-02 18:28:25 +0800

    feat: 优化 TS 类型代码，添加部分注释

--- src/_createFindIndexFunc.ts
+++ src/_createFindIndexFunc.ts
@@ -6,23 +6,19 @@ export interface FindIndexPredicate {
   (collectionItem: any, index: number, collection: FindIndexCollection): boolean
 }
 
-export interface CreateFindIndexFunc {
-  (direction?: 'first' | 'last'): (
-    collection: FindIndexCollection,
-    predicate: FindIndexPredicate,
-    fromIndex?: number
-  ) => number;
-}
-
 /**
  * 生成 findIndex 与 findLastIndex 函数
  * @param direction 遍历方向
  * @returns {Function} 返回一个 findIndex 函数 
  */
 
-const createFindIndexFunc: CreateFindIndexFunc = (direction = 'first') => {
+function createFindIndexFunc(direction = 'first') {
   const back = direction === 'last'
-  return (collection, predicate, fromIndex = 0) => {
+  return function (
+    collection: FindIndexCollection,
+    predicate: FindIndexPredicate,
+    fromIndex = 0
+  ) {
     if (!isArray(collection)) return -1
     
     for (let i = fromIndex; i < collection.length; i++) {
@@ -33,6 +29,6 @@ const createFindIndexFunc: CreateFindIndexFunc = (direction = 'first') => {
     }
     return -1
   }
-}
+} 
 
 export default createFindIndexFunc
--- src/_hasToStringTag.ts
+++ src/_hasToStringTag.ts
@@ -1,3 +1,10 @@
+/**
+ * 判断该值的 toString 是否为 object [name] 类型
+ * 
+ * @param value 要判断的值
+ * @param name 该值类型的字符串
+ * @returns {boolean} 返回判断结果
+ */
 const hasToStringTag = (value: unknown, name: string) => {
   return Object.prototype.toString.call(value) === `object ${name}`
 }
