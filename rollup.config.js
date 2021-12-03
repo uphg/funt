ec073e16388b231a6287e0e0e42a201aa9d6ab11
uphg <2633565885@qq.com>
2021-12-03 16:59:29 +0800

    ci: 生成 d.ts 类型声明文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,39 @@
 import esbuild from 'rollup-plugin-esbuild'
 import { terser } from 'rollup-plugin-terser'
+import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: `src/index.ts`,
   output: [
     {
       preserveModules: true, // 保留导入的模块为单独文件
-      dir: `dist/es`,
-      format: 'es'
+      dir: `dist`,
+      format: 'es',
+      sourcemap: true
     },
-    {
-      preserveModules: true, // 保留导入的模块为单独文件
-      dir: `dist/cjs`,
-      format: 'cjs'
-    },
-    {
-      name: 'funny',
-      file: 'dist/umd/index.js',
-      format: 'umd'
-    }
+    // {
+    //   preserveModules: true, // 保留导入的模块为单独文件
+    //   dir: `dist/es`,
+    //   format: 'es',
+    //   sourcemap: true
+    // },
+    // {
+    //   preserveModules: true, // 保留导入的模块为单独文件
+    //   dir: `dist/cjs`,
+    //   format: 'cjs',
+    //   sourcemap: true
+    // },
+    // {
+    //   name: 'funny',
+    //   file: 'dist/umd/index.js',
+    //   format: 'umd',
+    //   sourcemap: true
+    // }
   ],
   plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
     esbuild({
       include: /\.[jt]s$/,
       minify: process.env.NODE_ENV === 'production',
