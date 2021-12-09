9562da5b44a64af64afe19d6b63f4f27f3112cb8
uphg <2633565885@qq.com>
2021-12-09 10:58:37 +0800

    feat: 测试 lastIndexOf 函数，在 index 导出新增函数

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import * as funny from './index'
+import lastIndexOf from './lastIndexOf'
 
 console.log('funny')
 console.log(funny)
 
+console.log('lastIndexOf')
+console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 3))
+console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 5))
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
