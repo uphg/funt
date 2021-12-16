1dca8335ea0bc3bf922c6c39fd19fb89258fb249
uphg <2633565885@qq.com>
2021-12-16 16:48:41 +0800

    build: 修改 cjs 模块打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,7 @@ export default {
       preserveModules: true, // 保留导入的模块为单独文件
       dir: 'dist/cjs',
       format: 'cjs',
-      exports: 'named'
+      exports: 'auto'
     }
   ],
   plugins: [
