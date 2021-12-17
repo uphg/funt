5e4dc54a46e3f04f6ab46b3191d8a6769634bb46
uphg <2633565885@qq.com>
2021-12-17 11:05:28 +0800

    feat: 添加 MIN_SAFE_INTEGER 常量，测试 Map 判断兼容性

--- src/_common.ts
+++ src/_common.ts
@@ -1,3 +1,8 @@
-// JavaScript 可以显示的最大整数 
+// JavaScript 可显示的最大整数 
 export const MAX_SAFE_INTEGER = 9007199254740991
+
+// JavaScript 可显示的最小整数
+export const MIN_SAFE_INTEGER = -9007199254740991
+
+// 全局的 isNaN 函数
 export const _isNaN = isNaN
--- src/isNaN.ts
+++ src/isNaN.ts
@@ -1,6 +1,6 @@
 import isNumber from './isNumber';
 import { _isNaN } from './_common'
 
-export default function isNaN(value: unknown) {
+export default function isNaN(value: unknown): boolean {
   return Number.isNaN ? Number.isNaN(value) : isNumber(value) && _isNaN(value as number)
 }
