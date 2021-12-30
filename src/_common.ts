714f030c6b6082d6d0f7d6c93ba34977eddce251
uphg <2633565885@qq.com>
2021-12-30 13:14:35 +0800

    refactor: 删除无用的常量、注释

--- src/_common.ts
+++ src/_common.ts
@@ -1,13 +1,7 @@
-// JavaScript 可显示的最大整数 
+// 最大整数
 export const MAX_SAFE_INTEGER = 9007199254740991
 
-// JavaScript 可显示的最小整数
-export const MIN_SAFE_INTEGER = -9007199254740991
-
-// 生成 NaN 的常量
 export const NAN = 0 / 0
-
-// 全局的 isNaN 函数
 export const _isNaN = isNaN
 
 export const hasOwnProperty = Object.prototype.hasOwnProperty
@@ -18,4 +12,3 @@ export const _parseInt = parseInt
 
 export const NumberIsFinite = Number.isFinite
 export const NumberIsInteger = Number.isInteger
-
