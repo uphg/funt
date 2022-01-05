def96057c23c8101f36b80191076dc60e6b446ec
uphg <2633565885@qq.com>
2022-01-05 18:13:01 +0800

    优化节流函数

--- src/throttle.ts
+++ src/throttle.ts
@@ -1,19 +1,48 @@
+import now from './now'
+
 export default function throttle(
-  func: (...args: any) => any,
+  func: (...args: unknown[]) => unknown,
   wait: number
 ) {
-  let timerId: null | number | NodeJS.Timeout = null
+  let timerId: null | number | NodeJS.Timeout = null,
+    context: unknown,
+    args: unknown[] | null,
+    result: unknown
 
-  const result = function(...args: any) {
-    if (typeof timerId === 'number') return
-    
-    func.apply(this, args)
+  let previous = 0
 
-    timerId = setTimeout(() => {
-      clearTimeout(timerId as number)
-      timerId = null
-    }, wait)
+  const later = function() {
+    clearTimeout(timerId as number)
+
+    previous = now()
+    timerId = null
+    result = func.apply(context, args as unknown[])
+
+    if (!timerId) context = args = null
   }
 
-  return result
+  const throttled = function(..._args: any) {
+    const _now = now()
+    const remaining = wait - (_now - previous)
+    context = this
+    args = _args
+    // remaining > wait，客户端系统时间被调整过，马上执行 func 函数
+    if (remaining <= 0 || remaining > wait) {
+      if (timerId) {
+        clearTimeout(timerId as number)
+        timerId = null
+      }
+
+      previous = _now
+      result = func.apply(context, args as unknown[])
+
+      if (!timerId) context = args = null
+    } else if (!timerId) {
+      timerId = setTimeout(later, remaining)
+    }
+
+    return result
+  }
+
+  return throttled
 }
