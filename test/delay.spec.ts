5f1bb430a5250df4e67e208ee299e1f03007b61c
uphg <2633565885@qq.com>
2022-01-07 10:27:16 +0800

    test: 修正 delay 单元测试中的描述错误，添加是否为函数测试

--- test/delay.spec.ts
+++ test/delay.spec.ts
@@ -1,7 +1,11 @@
 import { delay } from '../src/index'
 
 describe('delay', () => {
-  it('is a function', (done) => {
+  it('is a function', () => {
+    expect(typeof delay === 'function').toBe(true)
+  })
+
+  it('delay call', (done) => {
     const mockFn = jest.fn()
     delay(mockFn, 100)
 
