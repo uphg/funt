5b57cacba05b777b4381e8f66a48d4aaf28cb46d
chen <1834805770@qq.com>
2022-06-12 22:34:00 +0800

    deploy

---
+++ assets/404.html.7903b92c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./app.3ff475ee.js";const r={};function _(t,c){return null}var n=e(r,[["render",_]]);export{n as default};
---
+++ assets/index.html.b5e97b4a.js
@@ -0,0 +1,13 @@
+import{_ as a,r as s,o as e,c as t,b as o,F as p,e as c}from"./app.3ff475ee.js";const r={},l=c(`<h2 id="\u5B89\u88C5" tabindex="-1"><a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a> \u5B89\u88C5</h2><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token function">npm</span> <span class="token function">install</span> funt
+<span class="token comment"># or yarn add funt</span>
+</code></pre></div><h2 id="\u5728\u9879\u76EE\u4E2D\u5BFC\u5165" tabindex="-1"><a class="header-anchor" href="#\u5728\u9879\u76EE\u4E2D\u5BFC\u5165" aria-hidden="true">#</a> \u5728\u9879\u76EE\u4E2D\u5BFC\u5165</h2><p>\u6309\u9700\u5BFC\u5165</p><div class="language-javascript ext-js"><pre class="language-javascript"><code><span class="token comment">// ES6 modules</span>
+<span class="token keyword">import</span> <span class="token punctuation">{</span> each<span class="token punctuation">,</span> find<span class="token punctuation">,</span> <span class="token operator">...</span> <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&#39;funt&#39;</span>
+
+<span class="token comment">// CommonJS modules</span>
+<span class="token keyword">const</span> <span class="token punctuation">{</span> each<span class="token punctuation">,</span> find<span class="token punctuation">,</span> <span class="token operator">...</span> <span class="token punctuation">}</span> <span class="token operator">=</span> <span class="token function">require</span><span class="token punctuation">(</span><span class="token string">&#39;funt&#39;</span><span class="token punctuation">)</span>
+</code></pre></div><p>\u652F\u6301\u5355\u4E2A\u65B9\u6CD5\u5F15\u5165</p><div class="language-javascript ext-js"><pre class="language-javascript"><code><span class="token comment">// ES6 modules</span>
+<span class="token keyword">import</span> each <span class="token keyword">from</span> <span class="token string">&#39;funt/each&#39;</span>
+
+<span class="token comment">// CommonJS modules</span>
+<span class="token keyword">const</span> each <span class="token operator">=</span> <span class="token function">require</span><span class="token punctuation">(</span><span class="token string">&#39;funt/each&#39;</span><span class="token punctuation">)</span>
+</code></pre></div>`,7);function u(i,d){const n=s("HomeHeader");return e(),t(p,null,[o(n),l],64)}var m=a(r,[["render",u]]);export{m as default};
