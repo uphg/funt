85e2c105d331c18c30d25eabaab537146d71adac
uphg <1834805770@qq.com>
2022-09-28 15:46:00 +0800

    refactor: 优化 toMoney 方法变量命名

--- src/toMoney.ts
+++ src/toMoney.ts
@@ -1,11 +1,11 @@
 function toMoney(value: unknown, mark = ',', defaultValue = '0') {
   if (!(typeof value === 'number' || typeof value === 'string')) return defaultValue
-  const number = typeof value === 'number' ? String(value) : value
-  const strings = number.split('.')
-  const int = strings[0]
-  const decimal = strings?.[1]
-  const length = int.length
-  let index = length
+  const strings = typeof value === 'number' ? String(value) : value
+  const numbers = strings.split('.')
+  const int = numbers[0]
+  const decimal = numbers?.[1]
+
+  let index = int.length
   let result = ''
   while (index > 0) {
     const part = int.slice(index > 3 ? index - 3 : 0, index)
